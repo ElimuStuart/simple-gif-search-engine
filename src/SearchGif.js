3caa92afa9f4edfe658d0ee467826e3dc8924248
Stuart Elimu <41069456+ElimuStuart@users.noreply.github.com>
2019-08-26 07:39:25 +0300

    Project completed
    Completed all componets but still getting an error calling api with sandbox

--- src/SearchGif.js
+++ src/SearchGif.js
@@ -9,10 +9,34 @@ class SearchGif extends Component {
     };
   }
 
+  onSubmit = event => {
+    event.preventDefault();
+    const apiKey = "dc6zaTOxFJmzC";
+    const url = `http://api.giphy.com/v1/gifs/search?q=${
+      this.state.term
+    }&api_key=${apiKey}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({ term: "", img: data.data[0].images.fied_height.url })
+      )
+      .catch(e => console.log("error", e));
+  };
+
+  onChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <p>Search for Gif</p>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.term} onChange={this.onChange} />
+          <button>Search</button>
+        </form>
+
+        <img src={this.state.img} alt={this.state.term} />
       </div>
     );
   }
